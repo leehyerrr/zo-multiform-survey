78c9faf65ee1c4221b15bb459e1bb6eefb31e551
leehyerrr <leehyerrr@gmail.com>
2026-04-11 20:15:03 +0900

    feat:react-router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -17,18 +17,6 @@ function App() {
               <Route path="responses" element={<div>응답</div>} />
             </Route>
           </Routes>
-          {/* <Tabs>
-          <TabList>
-            <Tab index={0}>tab1</Tab>
-            <Tab index={1}>tab2</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel index={0}>
-              <SectionListEditor />
-            </TabPanel>
-            <TabPanel index={1}>2222</TabPanel>
-          </TabPanels>
-        </Tabs> */}
         </SurveyStoreProvider>
       </MainLayout>
     </BrowserRouter>
